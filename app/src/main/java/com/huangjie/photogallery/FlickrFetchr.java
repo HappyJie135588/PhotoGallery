7e87f02c63bd314a4b03000db39546e4d23c1d5d
HuangJie <361125241@qq.com>
2016-08-22 17:48:37 +0800

    0822

--- app/src/main/java/com/huangjie/photogallery/FlickrFetchr.java
+++ app/src/main/java/com/huangjie/photogallery/FlickrFetchr.java
@@ -1,5 +1,8 @@
 package com.huangjie.photogallery;
 
+import android.net.Uri;
+import android.util.Log;
+
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -11,6 +14,10 @@ import java.net.URL;
  * Created by HuangJie on 2016/8/18.
  */
 public class FlickrFetchr {
+
+    private static final String TAG = "FlickrFetchr";
+    private static final String API_KEY = "8e1d2365a5047b91eb529e1d00681ef0";
+
     public byte[] getUrlBytes(String urlSpec) throws Exception {
         URL url = new URL(urlSpec);
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
@@ -34,5 +41,22 @@ public class FlickrFetchr {
     }
     public String getUrlString(String urlSpec) throws Exception {
      return new String(getUrlBytes(urlSpec));
+    }
+
+    public void fetchItems(){
+        try {
+            String url = Uri.parse("https://api.flickr.com/services/rest/")
+                    .buildUpon()
+                    .appendQueryParameter("method", "flickr.photos.getRecent")
+                    .appendQueryParameter("api_key", API_KEY)
+                    .appendQueryParameter("format", "json")
+                    .appendQueryParameter("nojsoncallback", "1")
+                    .appendQueryParameter("extras", "url_s")
+                    .build().toString();
+            String jsonString = getUrlString(url);
+            Log.i(TAG, "Received JSON: " + jsonString);
+        } catch (Exception e){
+            Log.e(TAG,"Failed to fetch items",e);
+        }
     }
 }
